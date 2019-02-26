d607ee148207d27c3ee7aeab7fbd12810617e354
Callysto Sysadmin <sysadmin@callysto.ca>
2019-02-26 19:55:55 +0000

    Updating Prod DNS to use OpenStack Designate

--- letsencrypt/prod/hook.sh
+++ letsencrypt/prod/hook.sh
@@ -1,176 +1,54 @@
 #!/usr/bin/env bash
 
-function has_propagated {
-    while [ "$#" -ge 2 ]; do
-        local RECORD_NAME="${1}"; shift
-        local TOKEN_VALUE="${1}"; shift
-        if [ ${#AUTH_NS[@]} -eq 0 ]; then
-            local RECORD_DOMAIN=$RECORD_NAME
-            declare -a iAUTH_NS
-            while [ -z "$iAUTH_NS" ]; do
-                RECORD_DOMAIN=$(echo "${RECORD_DOMAIN}" | cut -d'.' -f 2-)
-                iAUTH_NS=($(dig +short "${RECORD_DOMAIN}" IN CNAME))
-                if [ -n "$iAUTH_NS" ]; then
-                    unset iAUTH_NS && declare -a iAUTH_NS
-                    continue
-                fi
-                iAUTH_NS=($(dig +short "${RECORD_DOMAIN}" IN NS))
-            done
-        else
-           local iAUTH_NS=("${AUTH_NS[@]}")
-        fi
-        for NS in "${iAUTH_NS[@]}"; do
-            dig +short @"${NS}" "${RECORD_NAME}" IN TXT | grep -q "\"${TOKEN_VALUE}\"" || return 1
-        done
-        unset iAUTH_NS
-    done
-    return 0
-}
+if [ -z ${OPENRC_PATH+x} ]; then
+  echo "OPENRC_PATH var is unset in env or .envrc file"
+  exit 5
+fi
 
-function ocsp_update {
-    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
+deploy_challenge() {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 
-    # Get oscp response and shove it into a file, used for OCSP stapling.
-    #
-    # You only need this for old versions of of nginx that can't do this itself,
-    # or if your server is behind a proxy (eg nginx can't do OCSP via HTTP proxy).
+    # This hook is called once for every domain that needs to be
+    # validated, including any alternative names you may have listed.
     #
     # Parameters:
     # - DOMAIN
-    #   The primary domain name, i.e. the certificate common
-    #   name (CN).
-    # - KEYFILE
-    #   The path of the file containing the private key.
-    # - CERTFILE
-    #   The path of the file containing the signed certificate.
-    # - FULLCHAINFILE
-    #   The path of the file containing the full certificate chain.
-    # - CHAINFILE
-    #   The path of the file containing the intermediate certificate(s).
-    # - TIMESTAMP
-    #   Timestamp when the specified certificate was created.
+    #   The domain name (CN or subject alternative name) being
+    #   validated.
+    # - TOKEN_FILENAME
+    #   The name of the file containing the token to be served for HTTP
+    #   validation. Should be served by your web server as
+    #   /.well-known/acme-challenge/${TOKEN_FILENAME}.
+    # - TOKEN_VALUE
+    #   The token value that needs to be served for validation. For DNS
+    #   validation, this is what you want to put in the _acme-challenge
+    #   TXT record. For HTTP validation it is the value that is expected
+    #   be found in the $TOKEN_FILENAME file.
 
-    if [ -n "${OCSP_RESPONSE_FILE}" ]; then
-
-        if [ -z "${OCSP_HOST}" ]; then
-            OCSP_HOST="${http_proxy}" # eg http://foo.bar:3128/
-	    # strip protocol and path:
-	    OCSP_HOST="$(echo "$OCSP_HOST" | sed -E 's/(\w+:\/\/)((\w|\.)+:[0-9]+?)\/?.*/\2/')" # eg foo.bar:3128
-        fi
-
-        if [ -n "$VERBOSE" ]; then
-          echo "OCSP_HOST: $OCSP_HOST"
-          echo "http_proxy: $http_proxy"
-          echo "OCSP_RESPONSE_FILE: $OCSP_RESPONSE_FILE"
-          echo "CHAINFILE: $CHAINFILE"
-          echo "CERTFILE: $CERTFILE"
-          echo "command: openssl ocsp -noverify -no_nonce -respout \"${OCSP_RESPONSE_FILE}\" -issuer \"${CHAINFILE}\" -cert \"${CERTFILE}\" -host \"${OCSP_HOST}\" -path \"\$(openssl x509 -noout -ocsp_uri -in \"${CERTFILE}\")\" -CApath \"/etc/ssl/certs\""
-        fi
-
-        if [ -n "${OCSP_HOST}" ]; then
-            openssl ocsp -noverify -no_nonce -respout "${OCSP_RESPONSE_FILE}" -issuer "${CHAINFILE}" -cert "${CERTFILE}" -host "${OCSP_HOST}" -path "$(openssl x509 -noout -ocsp_uri -in "${CERTFILE}")" -CApath "/etc/ssl/certs"
-        else
-            openssl ocsp -noverify -no_nonce -respout "${OCSP_RESPONSE_FILE}" -issuer "${CHAINFILE}" -cert "${CERTFILE}" -path "$(openssl x509 -noout -ocsp_uri -in "${CERTFILE}")" -CApath "/etc/ssl/certs"
-        fi
-    fi
-}
-
-function oscp_update {
-    #oops :)
-    ocsp_update "$@"
-}
-
-function deploy_challenge {
-    local RECORDS=()
-    RECIPIENT=${RECIPIENT:-$(id -u -n)}
-    local FIRSTDOMAIN="${1}"
-    local SUBJECT="Let's Encrypt certificate renewal"
-    while (( "$#" >= 3 )); do
-        local DOMAIN="${1}"; shift
-        local TOKEN_FILENAME="${1}"; shift
-        local TOKEN_VALUE="${1}"; shift
-
-        # This hook is called once for every domain that needs to be
-        # validated, including any alternative names you may have listed.
-        #
-        # Parameters:
-        # - DOMAIN
-        #   The domain name (CN or subject alternative name) being
-        #   validated.
-        # - TOKEN_FILENAME
-        #   The name of the file containing the token to be served for HTTP
-        #   validation. Should be served by your web server as
-        #   /.well-known/acme-challenge/${TOKEN_FILENAME}.
-        # - TOKEN_VALUE
-        #   The token value that needs to be served for validation. For DNS
-        #   validation, this is what you want to put in the _acme-challenge
-        #   TXT record. For HTTP validation it is the value that is expected
-        #   be found in the $TOKEN_FILENAME file.
-
-        RECORD_NAME="_acme-challenge.${DOMAIN}"
-        RECORDS+=( ${RECORD_NAME} )
-        RECORDS+=( ${TOKEN_VALUE} )
-    done
-
-    read -d '' MESSAGE <<EOF
-The Let's Encrypt certificate for ${FIRSTDOMAIN} is about to expire.
-Before it can be renewed, ownership of the domain must be proven by
-responding to a challenge.
-
-Please deploy the following record(s) to validate ownership of ${FIRSTDOMAIN}:
-
-EOF
-    for (( i=0; i < "${#RECORDS[@]}"; i+=2 )); do
-        MESSAGE="$(printf '%s\n  %s. IN TXT %s\n' "$MESSAGE" "${RECORDS[$i]}" "${RECORDS[$(($i + 1))]}")"
-    done
-
-    echo "$MESSAGE" | mail -s "$SUBJECT" "$RECIPIENT"
-
-    echo " + Settling down for 10s..."
+    # Simple example: Use nsupdate with local named
+    # printf 'server 127.0.0.1\nupdate add _acme-challenge.%s 300 IN TXT "%s"\nsend\n' "${DOMAIN}" "${TOKEN_VALUE}" | nsupdate -k /var/run/named/session.key
+    source ${OPENRC_PATH}
+    openstack recordset delete ${DOMAIN}. _acme-challenge.${DOMAIN}. > /dev/null 2>&1;
+    openstack recordset create ${DOMAIN}. _acme-challenge.${DOMAIN}. --type TXT --record \"${TOKEN_VALUE}\";
     sleep 10
-
-    while ! has_propagated "${RECORDS[@]}"; do
-         echo " + DNS not propagated. Waiting 30s for record creation and replication..."
-         sleep 30
-    done
 }
 
-function clean_challenge {
-    local RECORDS=()
-    RECIPIENT=${RECIPIENT:-$(id -u -n)}
-    local FIRSTDOMAIN="${1}"
-    local SUBJECT="Let's Encrypt certificate renewal"
-    while (( "$#" >= 3 )); do
-        local DOMAIN="${1}"; shift
-        local TOKEN_FILENAME="${1}"; shift
-        local TOKEN_VALUE="${1}"; shift
+clean_challenge() {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 
-        # This hook is called after attempting to validate each domain,
-        # whether or not validation was successful. Here you can delete
-        # files or DNS records that are no longer needed.
-        #
-        # The parameters are the same as for deploy_challenge.
+    # This hook is called after attempting to validate each domain,
+    # whether or not validation was successful. Here you can delete
+    # files or DNS records that are no longer needed.
+    #
+    # The parameters are the same as for deploy_challenge.
 
-        RECORD_NAME="_acme-challenge.${DOMAIN}"
-        RECORDS+=( ${RECORD_NAME} )
-        RECORDS+=( ${TOKEN_VALUE} )
-    done
-
-    read -d '' MESSAGE <<EOF
-Progagation has completed for ${FIRSTDOMAIN}. The following record(s) can now be deleted:
-
-EOF
-
-    while (( "${#RECORDS}" >= 2 )); do
-        MESSAGE="$(printf '%s\n  %s. IN TXT %s\n' "$MESSAGE" "${RECORDS[0]}" "${RECORDS[1]}")"
-        RECORDS=( "${RECORDS[@]:2}" )
-    done
-
-    echo "$MESSAGE" | mail -s "$SUBJECT" "$RECIPIENT"
-
+    # Simple example: Use nsupdate with local named
+    # printf 'server 127.0.0.1\nupdate delete _acme-challenge.%s TXT "%s"\nsend\n' "${DOMAIN}" "${TOKEN_VALUE}" | nsupdate -k /var/run/named/session.key
+    source ${OPENRC_PATH}
+    openstack recordset delete ${DOMAIN}. _acme-challenge.${DOMAIN}. > /dev/null 2>&1;
 }
 
-function deploy_cert {
+deploy_cert() {
     local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
 
     # This hook is called once for each certificate that has been
@@ -192,10 +70,13 @@ function deploy_cert {
     # - TIMESTAMP
     #   Timestamp when the specified certificate was created.
 
-    oscp_update "$@"
+    # Simple example: Copy file to nginx config
+    # cp "${KEYFILE}" "${FULLCHAINFILE}" /etc/nginx/ssl/; chown -R nginx: /etc/nginx/ssl
+    # systemctl reload nginx
+    #cp "${KEYFILE}" "${FULLCHAINFILE}" ./terraform/dev/pki/
 }
 
-function unchanged_cert {
+unchanged_cert() {
     local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
 
     # This hook is called once for each certificate that is still
@@ -213,11 +94,85 @@ function unchanged_cert {
     #   The path of the file containing the full certificate chain.
     # - CHAINFILE
     #   The path of the file containing the intermediate certificate(s).
-
-    oscp_update "$@"
 }
 
-HANDLER=$1; shift
-if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge|deploy_cert|unchanged_cert)$ ]]; then
+invalid_challenge() {
+    local DOMAIN="${1}" RESPONSE="${2}"
+
+    # This hook is called if the challenge response has failed, so domain
+    # owners can be aware and act accordingly.
+    #
+    # Parameters:
+    # - DOMAIN
+    #   The primary domain name, i.e. the certificate common
+    #   name (CN).
+    # - RESPONSE
+    #   The response that the verification server returned
+
+    # Simple example: Send mail to root
+    # printf "Subject: Validation of ${DOMAIN} failed!\n\nOh noez!" | sendmail root
+}
+
+request_failure() {
+    local STATUSCODE="${1}" REASON="${2}" REQTYPE="${3}" HEADERS="${4}"
+
+    # This hook is called when an HTTP request fails (e.g., when the ACME
+    # server is busy, returns an error, etc). It will be called upon any
+    # response code that does not start with '2'. Useful to alert admins
+    # about problems with requests.
+    #
+    # Parameters:
+    # - STATUSCODE
+    #   The HTML status code that originated the error.
+    # - REASON
+    #   The specified reason for the error.
+    # - REQTYPE
+    #   The kind of request that was made (GET, POST...)
+
+    # Simple example: Send mail to root
+    # printf "Subject: HTTP request failed failed!\n\nA http request failed with status ${STATUSCODE}!" | sendmail root
+}
+
+generate_csr() {
+    local DOMAIN="${1}" CERTDIR="${2}" ALTNAMES="${3}"
+
+    # This hook is called before any certificate signing operation takes place.
+    # It can be used to generate or fetch a certificate signing request with external
+    # tools.
+    # The output should be just the cerificate signing request formatted as PEM.
+    #
+    # Parameters:
+    # - DOMAIN
+    #   The primary domain as specified in domains.txt. This does not need to
+    #   match with the domains in the CSR, it's basically just the directory name.
+    # - CERTDIR
+    #   Certificate output directory for this particular certificate. Can be used
+    #   for storing additional files.
+    # - ALTNAMES
+    #   All domain names for the current certificate as specified in domains.txt.
+    #   Again, this doesn't need to match with the CSR, it's just there for convenience.
+
+    # Simple example: Look for pre-generated CSRs
+    # if [ -e "${CERTDIR}/pre-generated.csr" ]; then
+    #   cat "${CERTDIR}/pre-generated.csr"
+    # fi
+}
+
+startup_hook() {
+  # This hook is called before the cron command to do some initial tasks
+  # (e.g. starting a webserver).
+
+  :
+}
+
+exit_hook() {
+  # This hook is called at the end of the cron command and can be used to
+  # do some final (cleanup or other) tasks.
+
+  :
+}
+
+HANDLER="$1"; shift
+if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge|deploy_cert|unchanged_cert|invalid_challenge|request_failure|generate_csr|startup_hook|exit_hook)$ ]]; then
   "$HANDLER" "$@"
 fi
