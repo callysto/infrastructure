75e74b81d46a4ce66073ce5f92fcf7c1d7fa2ad4
Callysto Sysadmin <sysadmin@callysto.ca>
2020-02-04 02:44:57 +0000

    Support for encrypted volumes

--- terraform/modules/stats/volumes.tf
+++ terraform/modules/stats/volumes.tf
@@ -3,6 +3,8 @@ resource "openstack_blockstorage_volume_v2" "zfs" {
 
   name = "${format("%s-zfs-%02d", var.name, count.index+1)}"
   size = "${var.vol_zfs_size}"
+
+  volume_type = "encrypted"
 }
 
 # Determine the volume UUIDs, whether if existing ones were supplied
