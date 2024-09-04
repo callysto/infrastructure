aa417954db866500c5172061d803c7867255fe70
Cybera Sysadmin <sysadmin@cybera.ca>
2024-09-04 22:53:40 +0000

    update hub volumes for opentofu 1.8.1
    Signed-off-by: Cybera Sysadmin <sysadmin@cybera.ca>

--- terraform/modules/hub/volumes.tf
+++ terraform/modules/hub/volumes.tf
@@ -1,23 +1,23 @@
-resource "openstack_blockstorage_volume_v2" "zfs" {
+resource "openstack_blockstorage_volume_v3" "zfs" {
   count = "${length(var.existing_volumes) == 0 ? 2 : 0}"
 
   name = "${format("%s-zfs-%02d", var.name, count.index+1)}"
   size = "${var.vol_zfs_size}"
 
-  volume_type = "encrypted"
+  volume_type = "ssd"
 }
 
 # Determine the volume UUIDs, whether if existing ones were supplied
 # or if new ones were created.
 locals {
   vol_id_1 = "${length(var.existing_volumes) == 0 ?
-    element(concat(openstack_blockstorage_volume_v2.zfs.*.id, list("")), 0) :
-    element(concat(var.existing_volumes, list("")), 0)
+    element(concat(openstack_blockstorage_volume_v3.zfs.*.id, tolist([""])), 0) :
+    element(concat(var.existing_volumes, tolist([""])), 0)
   }"
 
   vol_id_2 = "${length(var.existing_volumes) == 0 ?
-    element(concat(openstack_blockstorage_volume_v2.zfs.*.id, list("")), 1) :
-    element(concat(var.existing_volumes, list("")), 1)
+    element(concat(openstack_blockstorage_volume_v3.zfs.*.id, tolist([""])), 1) :
+    element(concat(var.existing_volumes, tolist([""])), 1)
   }"
 }
 
@@ -29,7 +29,7 @@ resource "openstack_compute_volume_attach_v2" "zfs_1" {
 }
 
 resource "openstack_compute_volume_attach_v2" "zfs_2" {
-  depends_on = ["openstack_compute_volume_attach_v2.zfs_1"]
+  depends_on = [openstack_compute_volume_attach_v2.zfs_1]
 
   instance_id = "${openstack_compute_instance_v2.instance.id}"
   volume_id   = "${local.vol_id_2}"
